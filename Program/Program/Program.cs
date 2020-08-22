7dee1fb1b339ebaec2f973ea9e8a251287949258
erik <lapizcraft4@gmail.com>
2020-08-22 14:03:41 -0700

    d

---
+++ Program/Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new Queue<int>();
+            string answer = "";
+            int inpit = 0;
+
+
+            //values.Enqueue(1);
+            //values.Enqueue(2);
+            //values.Enqueue(3);
+
+            //values.Peek();
+
+            //values.Dequeue();
+
+            //values.Peek();
+
+            //values.Enqueue(3);
+        }
+    }
+}
---
+++ Program/Program/Queue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Queue <T>
+    {
+        const int defaultSize = 4;
+        public T[] values = new T[defaultSize];
+        int index = 0;
+        public int Count => index;
+
+        public void Resize()
+        {
+            T[] temp = new T[values.Length*2];          
+            for (int i = 0; i < index; i++)
+            {
+                temp[i] = values[i];
+            }
+            values = temp;
+        }
+
+        public void Enqueue(T value)
+        {
+            if (index == values.Length)
+            {
+                Resize();
+            }
+            values[index] = value;
+            index++;
+        }
+
+        public void Dequeue()
+        {
+            index--;
+        }
+
+        public void Peek()
+        {
+            Console.WriteLine(values[index]);
+        }
+    }
+}
